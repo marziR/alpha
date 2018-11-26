5d4cc38920ac9dedcdee9dd603df94be591a0188
marziR <42847903+marziR@users.noreply.github.com>
2018-11-26 10:26:18 -0800

    Delete Filesize.sh

--- Filesize.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter a file path: " filepath
-#eval $filepath 
-touch $filepath
-if [ $? -eq 0 ]; then
-FILESIZE=$(stat -c%s "$filepath")
-echo "Size of $filepath = $FILESIZE bytes."
-else
-echo "could not find the file"
-fi
